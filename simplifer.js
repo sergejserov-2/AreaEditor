4da901b44dedb74db05b6a5379fcd2ace506ef6f
sergejserov-2 <139491645+sergejserov-2@users.noreply.github.com>
2026-04-19 02:40:05 +0300

    Update and rename stabilizer.js to simplifer.js

--- simplifer.js
+++ simplifer.js
@@ -1,10 +1,16 @@
+// SIMPLE DISTANCE-BASED SIMPLIFIER (v1)
+
 function distance(a, b) {
   const dx = a[0] - b[0];
   const dy = a[1] - b[1];
   return Math.sqrt(dx * dx + dy * dy);
 }
 
-export function stabilizePoints(points, minDistance = 0.01) {
+// NOTE:
+// это пока НЕ full simplification (не Douglas-Peucker)
+// это first-level simplification = filtering + thinning
+
+export function simplifyPoints(points, minDistance = 0.01) {
   if (!points.length) return [];
 
   const result = [points[0]];
